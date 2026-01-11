c5c17cb8bbcb4616e93e66bd5f4a83c2903e5a47
sasha08qwe <postogrenka@yandex.ru>
2026-01-12 02:07:32 +0300

    Update http.go

--- awesomeProject7/internal/api/http.go
+++ awesomeProject7/internal/api/http.go
@@ -1,11 +1,21 @@
 package api
 
-import "awesomeProject7/internal/service"
+import (
+	"errors"
+
+	"awesomeProject7/internal/service"
+)
 
 type HTTPHandler struct {
 	service *service.RatesService
 }
 
-func NewHTTPHandler(service *service.RatesService) *HTTPHandler {
-	return &HTTPHandler{service: service}
+func NewHTTPHandler(service *service.RatesService) (*HTTPHandler, error) {
+	if service == nil {
+		return nil, errors.New("rates service is nil")
+	}
+
+	return &HTTPHandler{
+		service: service,
+	}, nil
 }
